0b30b277046a9727c3edeaefd55c662923c4e2e5
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-06-02 13:46:35 -0300

    Add links to sort by: stars, updated, forks
    Cache repos in memory for 30 minutes, the repos are cached per sorting
    type. This should suffice for now, but it might be interesting to do the
    sorting on our side in a next iteration.

--- app.cr
+++ app.cr
@@ -2,6 +2,10 @@ require "frank"
 require "http/client"
 require "./views/index"
 require "./models/github_repos"
+require "./models/time_cache"
+
+SORT_OPTIONS = {"stars", "updated", "forks"}
+REPOS_CACHE = TimeCache(String, GithubRepos).new(30.minutes)
 
 def headers
   headers = HTTP::Headers.new
@@ -9,13 +13,21 @@ def headers
   headers
 end
 
-def crystal_repos
+def crystal_repos(sort)
   client = HTTP::Client.new("api.github.com", 443, true)
-  response = client.get("/search/repositories?q=language:crystal&per_page=100", headers)
+  response = client.get("/search/repositories?q=language:crystal&per_page=100&sort=#{sort}", headers)
   GithubRepos.from_json(response.body)
 end
 
+def fetch_sort(context)
+  sort = context.params["sort"]? || "stars"
+  sort = "stars" unless SORT_OPTIONS.includes?(sort)
+  sort
+end
+
 get "/" do |context|
+  sort = fetch_sort(context)
   context.response.content_type = "text/html"
-  Views::Index.new crystal_repos
+  repos = REPOS_CACHE.fetch(sort) { crystal_repos(sort) }
+  Views::Index.new repos, sort
 end
--- models/github_repos.cr
+++ models/github_repos.cr
@@ -6,8 +6,4 @@ class GithubRepos
     total_count: { type: Int32 },
     items: { type: Array(GithubRepo) }
   })
-
-  def items
-    @items.sort_by(&.watchers_count).reverse
-  end
 end
